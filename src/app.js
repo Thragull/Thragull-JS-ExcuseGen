7600189eefaf5cc73ca915a896eb73894d2677dc
Thragull <146970246+Thragull@users.noreply.github.com>
2024-01-14 19:54:50 +0000

    Creando pagina de Excuses Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function randomNumber(length) {
+    return Math.floor(Math.random() * length);
+  }
+  let sentence = "";
+  sentence += who[randomNumber(who.length)];
+  sentence += " ";
+  sentence += action[randomNumber(action.length)];
+  sentence += " ";
+  sentence += what[randomNumber(what.length)];
+  sentence += " ";
+  sentence += when[randomNumber(when.length)];
+  sentence += ".";
+  document.getElementById("idExcuse").textContent = sentence;
+  console.log(sentence);
 };
